f8866efb48aaf7bc35d51061c64eadc3d3164b2c
lukeni64 <lukeni0064@gmail.com>
2024-09-25 11:49:02 -0600

    Create sep25notes.py

---
+++ week5_lists_queues_stacks/sep25notes.py
@@ -0,0 +1,7 @@
+'''
+Lists and List-like Data Strutures:
+A list contains objects, while an array is adjacent memory.
+Arrays run much quicker than lists.
+Queue - FIFO, first in first out
+Stack - LIFO, last in first out
+'''
