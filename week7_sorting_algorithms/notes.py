28e522824b4d26a8a255013d6ab4e6734895f832
lukeni64 <lukeni0064@gmail.com>
2024-10-14 11:45:32 -0600

    class

---
+++ week7_sorting_algorithms/notes.py
@@ -0,0 +1,6 @@
+#ways to speed up your programs:
+#PP => Parallelize your Program
+#DD => Divide and Conquer your Data
+
+#Merge sort has a big O notation of n*log2(n)
+#Quick sort uses recursion and calls itself twice
